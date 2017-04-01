1532e6407b6b1ca7b87b98efb4c7ec6010f96ac0
AndEd <zx3211226@gmail.com>
2017-04-01 09:12:04 +0900

    Create CancelItemFrame.php

---
+++ Source/src/meta/CancelItemFrame.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace meta;
+
+use pocketmine\event\block\ItemFrameDropItemEvent;
+use pocketmine\plugin\pluginBase;
+use pocketmine\event\Listener;
+
+class CancelItemFrame extends PluginBase implements Listener {
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents( $this, $this);
+	}
+	public function onBreakItem(ItemFrameDropItemEvent $ev) {
+		if (!$ev->getPlayer->isOp()) {
+			$ev->setCancelled();
+		}
+	}
+}
